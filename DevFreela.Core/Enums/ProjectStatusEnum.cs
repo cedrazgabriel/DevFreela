576a1aa830e55f92480474408eb207f754ba3676
Gabriel Cedraz <cedrazdev@gmail.com>
2023-09-21 22:32:52 -0300

    implement core

---
+++ DevFreela.Core/Enums/ProjectStatusEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Enums
+{
+    public enum ProjectStatusEnum
+    {
+        Created,
+        InProgressed,
+        Suspended,
+        Canceled,
+        Finished
+    }
+}
